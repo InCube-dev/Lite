5e5b19886bb55be62d3410062ce6a0148958ab7d
Silke <53826868+blackysky@users.noreply.github.com>
2023-10-07 20:21:37 +0200

    Добавлен Lombok, интегрирован в Main класс

--- src/main/java/ru/incube/lite/Main.java
+++ src/main/java/ru/incube/lite/Main.java
@@ -1,7 +1,10 @@
 package ru.incube.lite;
 
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@Getter
+@SuppressWarnings({"deprecation", "ConstantConditions"})
 public final class Main extends JavaPlugin {
 
     @Override
